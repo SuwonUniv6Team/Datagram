5887d9762ef61de47d1fd264e91e28d59bed966f
ghtfy <skb88872@gmail.com>
2026-05-16 15:34:27 +0900

    하단부 트랙바 슬라이더, 제어 버튼, 로그 출력창 UI 디자인.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
             this.btnLoad = new System.Windows.Forms.Button();
             this.picMain = new System.Windows.Forms.PictureBox();
             this.prgAngle = new System.Windows.Forms.ProgressBar();
@@ -37,16 +37,22 @@
             this.lblAngleName = new System.Windows.Forms.Label();
             this.lblThrottleName = new System.Windows.Forms.Label();
             this.lstFrames = new System.Windows.Forms.ListBox();
+            this.trackFrame = new System.Windows.Forms.TrackBar();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackFrame)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtPath
             // 
-            this.textBox1.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.textBox1.Location = new System.Drawing.Point(143, 23);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(808, 39);
-            this.textBox1.TabIndex = 0;
+            this.txtPath.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPath.Location = new System.Drawing.Point(143, 23);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(808, 39);
+            this.txtPath.TabIndex = 0;
             // 
             // btnLoad
             // 
@@ -87,7 +93,7 @@
             // 
             this.lblMain.AutoSize = true;
             this.lblMain.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lblMain.Location = new System.Drawing.Point(7, 32);
+            this.lblMain.Location = new System.Drawing.Point(12, 32);
             this.lblMain.Name = "lblMain";
             this.lblMain.Size = new System.Drawing.Size(117, 20);
             this.lblMain.TabIndex = 5;
@@ -122,12 +128,68 @@
             this.lstFrames.Size = new System.Drawing.Size(311, 229);
             this.lstFrames.TabIndex = 8;
             // 
+            // trackFrame
+            // 
+            this.trackFrame.Location = new System.Drawing.Point(12, 468);
+            this.trackFrame.Name = "trackFrame";
+            this.trackFrame.Size = new System.Drawing.Size(1259, 56);
+            this.trackFrame.TabIndex = 9;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.BackColor = System.Drawing.Color.LightGray;
+            this.btnFilter.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnFilter.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnFilter.Location = new System.Drawing.Point(12, 530);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(153, 60);
+            this.btnFilter.TabIndex = 10;
+            this.btnFilter.Text = "데이터 필터링";
+            this.btnFilter.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.LightGray;
+            this.btnDelete.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDelete.ForeColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(171, 530);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(143, 60);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "프레임 삭제";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnTrain.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnTrain.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnTrain.Location = new System.Drawing.Point(1041, 530);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(219, 60);
+            this.btnTrain.TabIndex = 12;
+            this.btnTrain.Text = "AI 학습 시작";
+            this.btnTrain.UseVisualStyleBackColor = false;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 614);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(1248, 106);
+            this.txtLog.TabIndex = 13;
+            this.txtLog.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(1283, 601);
+            this.ClientSize = new System.Drawing.Size(1283, 732);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnTrain);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.trackFrame);
             this.Controls.Add(this.lstFrames);
             this.Controls.Add(this.lblThrottleName);
             this.Controls.Add(this.lblAngleName);
@@ -136,10 +198,11 @@
             this.Controls.Add(this.prgAngle);
             this.Controls.Add(this.picMain);
             this.Controls.Add(this.btnLoad);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPath);
             this.Name = "Form1";
             this.Text = "Datagram";
             ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackFrame)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,7 +210,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPath;
         private System.Windows.Forms.Button btnLoad;
         private System.Windows.Forms.PictureBox picMain;
         private System.Windows.Forms.ProgressBar prgAngle;
@@ -156,6 +219,11 @@
         private System.Windows.Forms.Label lblAngleName;
         private System.Windows.Forms.Label lblThrottleName;
         private System.Windows.Forms.ListBox lstFrames;
+        private System.Windows.Forms.TrackBar trackFrame;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.RichTextBox txtLog;
     }
 }
 
